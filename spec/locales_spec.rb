2886eafed4ff87b2e419baf04e58e6c3d9ae6de8
Daniel Uber <djuber@gmail.com>
2017-01-06 20:37:37 -0600

    clean up should syntax in locales spec

--- spec/locales_spec.rb
+++ spec/locales_spec.rb
@@ -7,24 +7,24 @@ describe Locales do
     let(:locale) { Locales::Locale.new(/\d{5}/) }
 
     it "parses postal codes with a regex" do
-      locale.parse_postal_code('10121').should == '10121'
+      expect(locale.parse_postal_code('10121')).to eq '10121'
     end
 
     it "returns nil if a postal code cannot be parsed" do
-      locale.parse_postal_code('abcde').should be_nil 
+      expect(locale.parse_postal_code('abcde')).to be_nil 
     end
   end
 
   it "Uses US Locale by default" do
-    Locales.current.should == Locales::UNITED_STATES
+    expect(Locales.current).to eq Locales::UNITED_STATES
   end
 
   it "can parse 5 digit zip codes" do
-    Locales.current.parse_postal_code('zip is 12345').should == '12345'
+    expect(Locales.current.parse_postal_code('zip is 12345')).to eq '12345'
   end
 
   it "allows the locale to be changed" do
     Locales.current = Locales::UNITED_KINGDOM
-    Locales.current.should == Locales::UNITED_KINGDOM
+    expect(Locales.current).to eq Locales::UNITED_KINGDOM
   end
 end
