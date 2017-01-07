9efc25850808252989d3a7b5e9d75819215d91e2
Daniel Uber <djuber@gmail.com>
2017-01-07 09:08:28 -0600

    added config skeleton as example

---
+++ example.twitter.config.rb
@@ -0,0 +1,9 @@
+# twitter api secrets for twits
+# I load this from /etc/twitter.config.rb, modify lib/user.rb to load from somewhere else.
+
+TWITTER_CONSUMER_KEY = 'KEYKEYKEY'
+TWITTER_CONSUMER_SECRET = 'SECRETSECRETSECRET'
+TWITTER_OWNER_ID = 'OWNEROWNER'
+
+TWITTER_ACCESS_TOKEN = 'TOKENTOKEN'
+TWITTER_ACCESS_TOKEN_SECRET = 'SECRETTOKEN'
